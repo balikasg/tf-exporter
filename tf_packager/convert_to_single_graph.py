e9c05395fd535ad10f7db24bdb4f08d274123d32
Georgios Balikas <geompalik@hotmail.com>
2022-10-11 16:51:10 +0200

    black it (#2)

--- tf_packager/convert_to_single_graph.py
+++ tf_packager/convert_to_single_graph.py
@@ -26,13 +26,14 @@ MODEL_MAPPING = {
     "sentence_transformers.models.Transformer": "tf_packager.models.Transformer",
     "sentence_transformers.models.Normalize": "tf_packager.models.Normalize",
     "sentence_transformers.models.Pooling": "tf_packager.models.Pooling",
-    "sentence_transformers.models.Dense": "tf_packager.models.TfDense"
+    "sentence_transformers.models.Dense": "tf_packager.models.TfDense",
 }
 
 
 class SentenceTransformerPackager(tf.keras.layers.Layer):
     """Class to convert a Sentence Transformer to a TensorFlow model
-     with its tokenizer packed in the same graph"""
+    with its tokenizer packed in the same graph"""
+
     def __init__(self, model_name_or_path):
         """Initialization.
         Parameters
@@ -41,33 +42,53 @@ class SentenceTransformerPackager(tf.keras.layers.Layer):
             Path to the Sentence Transformer model or name of the model
         """
         super().__init__()
-        modules_path = self.get_sentence_transformer_filepath(base_path=model_name_or_path,
-                                                              filename="modules.json")
+        modules_path = self.get_sentence_transformer_filepath(
+            base_path=model_name_or_path, filename="modules.json"
+        )
         modules = load_json(modules_path)
         self.modules = []
         for module in modules:
             LOG.info(module)
-            current = MODEL_MAPPING[module['type']]
+            current = MODEL_MAPPING[module["type"]]
             current_module = import_from_string(current)
-            current_module_initialized = current_module.load(modules_path.parents[0] / module['path'])
+            current_module_initialized = current_module.load(
+                modules_path.parents[0] / module["path"]
+            )
             self.modules.append(current_module_initialized)
 
-    def get_sentence_transformer_filepath(self, filename, base_path, fail_if_not_found=False):
+    def get_sentence_transformer_filepath(
+        self, filename, base_path, fail_if_not_found=False
+    ):
         """Given a `filename` and a `base_path` looks to find the file under the path.
         If not found, returns None. Used because in different sentence-transformers versions
         some files are saved under different locations."""
-        if not Path(base_path).exists():  # The user provided a model name and not a path to model
+        if not Path(
+            base_path
+        ).exists():  # The user provided a model name and not a path to model
             from torch.hub import _get_torch_home
+
             torch_cache_home = _get_torch_home()
-            base_path = Path(torch_cache_home) / 'sentence_transformers' \
-                        / "_".join([base_path.replace("/", "_")])
-        file_location = list(Path(base_path).rglob(filename))  # rglob = recursively glob
-        assert len(file_location) in (0, 1)  # Files either exists once (by ST design) or not
+            base_path = (
+                Path(torch_cache_home)
+                / "sentence_transformers"
+                / "_".join([base_path.replace("/", "_")])
+            )
+        file_location = list(
+            Path(base_path).rglob(filename)
+        )  # rglob = recursively glob
+        assert len(file_location) in (
+            0,
+            1,
+        )  # Files either exists once (by ST design) or not
         if file_location:
-            return file_location[0]  # Sent. Transformer models dy design have a single such file
+            return file_location[
+                0
+            ]  # Sent. Transformer models dy design have a single such file
         if fail_if_not_found:  # We did not find the file and we should fail
-            raise FileNotFoundError(f"Not found {filename} at {base_path} recursively."
-                                    f" This is unexpected for sentence-transformers")
+            raise FileNotFoundError(
+                f"Not found {filename} at {base_path} recursively."
+                f" This is unexpected for sentence-transformers"
+            )
         return None
 
     def call(self, batch):
@@ -81,8 +102,7 @@ class ModelConverter:
     """Class to convert a Tranformer Model that consist of tokenizer, a transformers model and
     potentially other part to a single TensorFlow graph"""
 
-    def __init__(self, model_name_or_path,
-                 output_dir):
+    def __init__(self, model_name_or_path, output_dir):
         """Initialization.
         Parameters
         ----------
@@ -93,7 +113,7 @@ class ModelConverter:
         """
         self.model_name_or_path = model_name_or_path
         self.embedding_size = None
-        self.tf_output_dir = Path(output_dir) / 'tf_model'
+        self.tf_output_dir = Path(output_dir) / "tf_model"
 
     def convert_pytorch_to_tensorflow(self, input_test):
         """Converts sentence transformer pytorch model to tensorflow. The
@@ -105,31 +125,41 @@ class ModelConverter:
             str to encode and to test the models
         """
         # Get the sentence-transformers predictions for comparison
-        out_sentence_transformer_pt = self.get_prediction_with_sentence_tranformers(input_test)
+        out_sentence_transformer_pt = self.get_prediction_with_sentence_tranformers(
+            input_test
+        )
 
         # Loading the PyTorch model from Hugging Face and convert it to TF
         embeddings_keras_model, keras_model = self.get_complete_model(input_test)
-        assert_almost_equal(embeddings_keras_model.numpy()[0],
-                            out_sentence_transformer_pt,
-                            decimal=5,
-                            err_msg="embedding differs across different models for one input")
-        LOG.info('\u2713 Sentence Transformer pytorch model embeddings are almost equal with '
-                 'CompleteSentenceTransformer model ')
+        assert_almost_equal(
+            embeddings_keras_model.numpy()[0],
+            out_sentence_transformer_pt,
+            decimal=5,
+            err_msg="embedding differs across different models for one input",
+        )
+        LOG.info(
+            "\u2713 Sentence Transformer pytorch model embeddings are almost equal with "
+            "CompleteSentenceTransformer model "
+        )
 
         # Saving the TensorFlow model
         self.tf_output_dir.mkdir(exist_ok=True, parents=True)
 
         keras_model.save(self.tf_output_dir)
-        LOG.info('Saving `%s` TF model' % self.tf_output_dir)
+        LOG.info("Saving `%s` TF model" % self.tf_output_dir)
 
-        LOG.info('Loading `%s` TF saved_model' % self.tf_output_dir)
+        LOG.info("Loading `%s` TF saved_model" % self.tf_output_dir)
         embeddings_keras_loaded_model = self.get_predictions_from_tf_model(input_test)
-        assert_almost_equal(embeddings_keras_model.numpy(),
-                            embeddings_keras_loaded_model.numpy(),
-                            decimal=5,
-                            err_msg="Embeddings differ across different models for one input")
+        assert_almost_equal(
+            embeddings_keras_model.numpy(),
+            embeddings_keras_loaded_model.numpy(),
+            decimal=5,
+            err_msg="Embeddings differ across different models for one input",
+        )
 
-        LOG.info('\u2713 Embeddings of the original and loaded TensorFlow models are almost equal!')
+        LOG.info(
+            "\u2713 Embeddings of the original and loaded TensorFlow models are almost equal!"
+        )
 
     def get_predictions_from_tf_model(self, input_test):
         """Loads and gets predictions from a persisted tensorflow model
@@ -142,7 +172,8 @@ class ModelConverter:
         # https://www.tensorflow.org/api_docs/python/tf/saved_model/load
         serving_signature = keras_loaded_model.signatures["serving_default"]
         embeddings_keras_loaded_model = serving_signature(
-            input_sequence=tf.constant([[input_test]]))
+            input_sequence=tf.constant([[input_test]])
+        )
         return embeddings_keras_loaded_model[KERAS_OUTPUT_NAME]
 
     def get_complete_model(self, input_test):
@@ -154,7 +185,9 @@ class ModelConverter:
         """
         # Define model
         complete_model = SentenceTransformerPackager(self.model_name_or_path)
-        inputs = tf.keras.layers.Input(shape=(1,), dtype=tf.string, name=KERAS_INPUT_NAME)
+        inputs = tf.keras.layers.Input(
+            shape=(1,), dtype=tf.string, name=KERAS_INPUT_NAME
+        )
         outputs = complete_model(inputs)
         model = tf.keras.Model(inputs, outputs)
         # Test with `input_test`
@@ -175,18 +208,28 @@ class ModelConverter:
 
 def get_parser():
     """Argument parser"""
-    parser = argparse.ArgumentParser(description='Convert sentence transformer PyTorch model '
-                                                 'to Keras Tensorflow model')
-    parser.add_argument('--model-name-or-path', type=str,
-                        default='sentence-transformers/nq-distilbert-base-v1',
-                        help='Sentence Transformer model name or path')
-    parser.add_argument('--output-dir', type=str, default='models',
-                        help='Directory to save the tensorflow model as a single graph')
+    parser = argparse.ArgumentParser(
+        description="Convert sentence transformer PyTorch model "
+        "to Keras Tensorflow model"
+    )
+    parser.add_argument(
+        "--model-name-or-path",
+        type=str,
+        default="sentence-transformers/nq-distilbert-base-v1",
+        help="Sentence Transformer model name or path",
+    )
+    parser.add_argument(
+        "--output-dir",
+        type=str,
+        default="models",
+        help="Directory to save the tensorflow model as a single graph",
+    )
     return parser
 
 
 if __name__ == "__main__":
     args = get_parser().parse_args()
-    converter = ModelConverter(model_name_or_path=args.model_name_or_path,
-                               output_dir=args.output_dir)
-    converter.convert_pytorch_to_tensorflow(input_test='This is a test')
+    converter = ModelConverter(
+        model_name_or_path=args.model_name_or_path, output_dir=args.output_dir
+    )
+    converter.convert_pytorch_to_tensorflow(input_test="This is a test")
